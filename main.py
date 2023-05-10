6ccc020525e2868e68f0509541f438226b090da9
andrew <ajacoboc@unsa.edu.pe>
2023-05-10 15:48:52 -0500

    falla en la desunga opcion

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import time
-from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 import zipfile
 import os
+import time
 import pandas as pd
 import xlrd
+from db import base_datos
+
 #sistemas@emaransac.com
 #Sm~18jn57
 
@@ -22,6 +24,9 @@ class scrap:
         self.s = Service("/home/yerson/Downloads/chromedriver_linux64/chromedriver")
         self.ruta_descarga = "/home/yerson/Downloads/"
         """
+
+        self.db = base_datos()
+
         self.file_name = ""
 
         chromeOptions = Options()
@@ -42,16 +47,6 @@ class scrap:
         #self.servicio = Service(self.s)
         #self.driver = webdriver.Chrome()
         self.action = ActionChains(self.driver)
-
-    def insertar(Ci, Np, Cb, St, Ps, Ub):
-        sql = "INSERT INTO `plazavea`(`codigo_interno`, `nom_producto`, `codigo_barras`, `stock`, `presentacion`, `ubicacion`)" \
-              " VALUES (%s,%s,%s,%s,%s,%s)"
-        val = (Ci, Np, Cb, St, Ps, Ub)
-
-
-        #self.cursor.execute(sql, val)
-        #self.cnx.commit()
-
 
     def getFileName(self):
         for archivo in os.listdir(self.ruta_descarga):
@@ -86,8 +81,16 @@ class scrap:
 
     def login(self):
         self.driver.get('https://b2b.intercorpretail.pe')
-        self.driver.maximize_window()
+        #self.driver.maximize_window()
+
+        #self.driver.get("https://www.seleniumhq.org/download/");
+        # get user Agent with execute_script
+        #a = self.driver.execute_script("return navigator.userAgent")
+        #print("User agent:")
+        #print(a)
+
         #self.driver.minimize_window()
+
         dropdownbox = self.driver.find_elements(by=By.TAG_NAME, value='Option')
         i = 0;
         while i < len(dropdownbox):
@@ -105,200 +108,184 @@ class scrap:
         contrasenia.send_keys("VENTAS2023")
         login = self.driver.find_element(by=By.CLASS_NAME,value='pf-c-button.pf-m-primary.pf-m-block.btn-lg')
         login.click()
-        time.sleep(5)
-        print("----> login <----")
-
-    def log(self):
-        self.driver.get('https://b2b.intercorpretail.pe')
-        self.driver.maximize_window()
-        options = self.driver.find_element(by=By.XPATH,value='//*[@id="url"]')
-        self.action.click(options).perform()
         time.sleep(3)
-        print("options")
-
-        dropdownbox = self.driver.find_elements(by=By.TAG_NAME, value='Option')
-        i = 0;
-        while i < len(dropdownbox):
-            if dropdownbox[i].text == 'Supermercados Peruanos S.A.':
-                dropdownbox[i].click()
-                break
-            i += 1
-
-        Ingresar = self.driver.find_element(by=By.CLASS_NAME,value='btn.btn-primary.btn-sm')
-        Ingresar.click()
-
-        email = self.driver.find_element(by=By.NAME,value='username')
-        email.send_keys("ventas@emaransac.com")
-        time.sleep(1)
-        contrasenia = self.driver.find_element(by=By.NAME,value='password')
-        contrasenia.send_keys("VENTAS2023")
-        login = self.driver.find_element(by=By.CLASS_NAME,value='pf-c-button.pf-m-primary.pf-m-block.btn-lg')
-        login.click()
-        time.sleep(443)
-        print("----> login <----")
-
-
-        abast = self.driver.find_element(By.XPATH, value='/html/body')
-        self.action.click(abast).perform()
-        time.sleep(1)
-        print("abastecimiento")
-
-        local = self.driver.find_element(By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[2]/div/div/span[1]/span')
-        self.action.move_to_element(local)
-        self.action.click(local).perform()
-        time.sleep(2)
-        print("local")
-
-        generar = self.driver.find_element(by=By.CLASS_NAME,value='v-button.v-widget.btn-filter-search.v-button-btn-filter-search')
-        generar.click()
-        time.sleep(4)
-        print("generar")
-
+        print("[ >> Login << ]")
 
 
     def abastecimiento(self):
-        time.sleep(7)
-        abast = self.driver.find_element(By.XPATH, value='//*[@id="SupermercadosBBRecommercemain-1228722670"]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[1]/div/div/div[3]/div/span[3]/span[2]')
+        time.sleep(1)
+        triangle = self.driver.find_element(By.XPATH, value='//*[@id="SupermercadosBBRecommercemain-1228722670"]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[1]/div/div/div[3]/div/span[3]')
+        self.action.click(triangle).perform()
+        time.sleep(4)
+        print("[ >> 1. Init << ]")
+
+        abast = self.driver.find_element(By.XPATH, value='//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[2]/div/div/span[1]/span[2]')
         self.action.click(abast).perform()
         time.sleep(7)
-        print("abastecimiento")
+        print("[ >> 2. Abastecimiento << ]")
 
-        local = self.driver.find_element(By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[2]/div/div/span[1]/span')
+        local = self.driver.find_element(By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[3]/div/div/span[1]/span')
+                                                       #'//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[3]/div/div/span[2]/span'
         self.action.move_to_element(local)
         self.action.click(local).perform()
         time.sleep(6)
-        print("local")
+        print("[ >> 3. Indicadores Inv Local << ]")
 
         generar = self.driver.find_element(by=By.CLASS_NAME,value='v-button.v-widget.btn-filter-search.v-button-btn-filter-search')
         generar.click()
         time.sleep(6)
-        print("generar")
+        print("[ >> 4. Generar informe << ]")
 
         descargar = self.driver.find_element(by=By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670"]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[3]/div/div/div/div/div[2]/div/div/div/div/div/div/div[2]/div/div[1]/div/div/div/div/div[1]/div')
         descargar.click()
         time.sleep(6)
-        print("descargar")
+        print("[ >> 5. Descargar <<]")
 
         exel = self.driver.find_element(by=By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[3]/div/div/div[3]/div/div/div/div/div/div/div/div[2]/div/div/div/div/span[2]/label')
         exel.click()
         time.sleep(6)
-        print("exel")
+        print("[ >> 6. Formato << ]")
 
         archivo = self.driver.find_element(by=By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[3]/div/div/div[3]/div/div/div/div/div/div/div/div[3]/div/div/div[1]/div')
         archivo.click()
         time.sleep(6)
-        print("archivo")
+        print("[ >> 7. Archivo .xls << ]")
 
         zip = self.driver.find_element(by=By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[3]/div/div/div[3]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div/a/span')
         zip.click()
-        print("zip")
-
+        print("[ >> 8. Descarga .zip << ]")
         time.sleep(2)
 
     def detalleinv(self):
-        time.sleep(2)
-        abast = self.driver.find_element(By.XPATH, value='//*[@id="SupermercadosBBRecommercemain-1228722670"]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[1]/div/div/div[3]/div/span[3]/span[2]')
+        triangle = self.driver.find_element(By.XPATH, value='//*[@id="SupermercadosBBRecommercemain-1228722670"]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[1]/div/div/div[3]/div/span[3]')
+        self.action.click(triangle).perform()
+        time.sleep(1)
+        print("[ >> 1. Init << ]")
+
+        abast = self.driver.find_element(By.XPATH, value='//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[2]/div/div/span[1]/span[2]')
         self.action.click(abast).perform()
+        time.sleep(766)
+        print("[ >> 2. Abastecimiento.. << ]")
+
+        local = self.driver.find_element(By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[3]/div/div/span[2]/span')
+        self.action.move_to_element(local)
+        self.action.click(local).perform()
         time.sleep(2)
-        detalle = self.driver.find_element(By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[2]/div/div/span[2]/span')
-        self.action.move_to_element(detalle)
-        self.action.click(detalle).perform()
-        time.sleep(5)
+        print("[ >> 3. Detalle Inv << ]")
 
         generar = self.driver.find_element(by=By.CLASS_NAME,value='v-button.v-widget.btn-filter-search.v-button-btn-filter-search')
         generar.click()
         time.sleep(2)
-        print("Generando reporte")
+        print("[ >> 4. Generar informe << ]")
+
 
         Descarga = self.driver.find_element(by=By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670"]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[3]/div/div/div/div/div[2]/div/div/div/div/div/div/div[2]/div/div[1]/div/div/div[3]/div/div[1]/div')
         Descarga.click()
         time.sleep(2)
-        print("Descargando reporte")
+        print("[ >> 5. Descargar << ]")
+
 
         exel = self.driver.find_element(by=By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[2]/div/div/div[1]/div')
         exel.click()
         time.sleep(2)
-        print("exel formato")
+        print("[ >> 6. Formato << ]")
+
 
         Exel  = self.driver.find_element(by=By.XPATH,value= '//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[3]/div/div/div[3]/div/div/div/div/div/div/div/div[2]/div/div/div/div/span[2]/label')
         Exel.click()
+        print("[ >> 7. Archivo .xls << ]")
         time.sleep(2)
-        print("EXEL formato")
+
 
         Seleccionar = self.driver.find_element(by=By.XPATH,value= '//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[3]/div/div/div[3]/div/div/div/div/div/div/div/div[3]/div/div/div[1]/div')
         Seleccionar.click()
-        time.sleep(7)
-        print("Seleccionando archivo")
+        time.sleep(5)
+        print("[ >> 8. Seleccionar << ]")
+
 
         zip = self.driver.find_element(by=By.XPATH,value= '//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[3]/div/div/div[3]/div/div/div/div/div/div/div/div/div/div/div[2]')
         zip.click()
-        print("ZIP archivo")
+        print("[ >> 9. Descarga .zip << ]")
         time.sleep(2)
 
     def informeCv(self):
-        time.sleep(2)
+        time.sleep(1)
+        triangle = self.driver.find_element(By.XPATH, value='//*[@id="SupermercadosBBRecommercemain-1228722670"]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[1]/div/div/div[3]/div/span[3]')
+        self.action.click(triangle).perform()
+        time.sleep(4)
+        print("[ >> 1. Init << ]")
+
         abast = self.driver.find_element(By.XPATH, value='//*[@id="SupermercadosBBRecommercemain-1228722670"]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[1]/div/div/div[3]/div/span[3]/span[2]')
         self.action.click(abast).perform()
         time.sleep(2)
-        print("Abastecimiento")
+        print("[ >> 2. Abastecimiento << ]")
 
 
-        detalle = self.driver.find_element(By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[2]/div/div/span[3]/span')
+
+        detalle = self.driver.find_element(By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[3]/div/div/span[3]/span')
         self.action.move_to_element(detalle)
         self.action.click(detalle).perform()
         time.sleep(3)
-        print("Detalle")
+        print("[ >> 3. Detalle Inv << ]")
+
 
         generar = self.driver.find_element(by=By.CLASS_NAME,value='v-button.v-widget.btn-filter-search.v-button-btn-filter-search')
         generar.click()
         time.sleep(8)
-        print("Generando reporte")
+        print("[ >> 4. Generar informe << ]")
+
 
         Descarga = self.driver.find_element(by=By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670"]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[3]/div/div/div/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div/div[2]/div/div/div/div/div[1]/div/div/div[3]/div/div[1]/div')
         Descarga.click()
         time.sleep(3)
-        print("Descargando reporte")
+        print("[ >> 5. Descargar << ]")
+
 
         exel = self.driver.find_element(by=By.XPATH,value='//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[2]/div/div/div[1]/div')
         exel.click()
         time.sleep(3)
-        print("exel formato")
+        print("[ >> 6. Formato << ]")
+
 
         Exel  = self.driver.find_element(by=By.XPATH,value= '//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[3]/div/div/div[3]/div/div/div/div/div/div/div/div[2]/div/div/div/div/span[2]/label')
         Exel.click()
         time.sleep(3)
-        print("EXEL formato")
+        print("[ >> 7. Archivo .xls << ]")
+
 
         Seleccionar = self.driver.find_element(by=By.XPATH,value= '//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[3]/div/div/div[3]/div/div/div/div/div/div/div/div[3]/div/div/div[1]/div')
         Seleccionar.click()
         time.sleep(6)
-        print("Seleccionando archivo")
+        print("[ >> 8. Seleccionar << ]")
+
 
         zip = self.driver.find_element(by=By.XPATH,value= '//*[@id="SupermercadosBBRecommercemain-1228722670-overlays"]/div[3]/div/div/div[3]/div/div/div/div/div/div/div/div/div/div/div[2]')
         zip.click()
-        print("ZIP archivo")
+        print("[ >> 9. Descarga .zip << ]")
+
         time.sleep(3)
     def upload(self):
-        db.unsip()
-        db.file_df()
+        sc.unsip()
+        sc.file_df()
 
     def init(self):
         self.login()
-        print("Iniciando abastecimiento")
-        self.abastecimiento()
-        self.file_df()
-        time.sleep(5)
 
-        print("Iniciando detalle inventario")
+        #print("[ >> 1.Abastecimiento << ]")
+        #self.abastecimiento()
+        #self.file_df()
+        #time.sleep(5)
+
+        print("[ >> 2.Detalle Inv << ]")
         self.detalleinv()
         self.file_df()
         time.sleep(5)
 
 
-        print("Iniciando informe cv")
-        self.informeCv()
-        self.file_df()
-        print("--FIN--")
+        #print("[ >> 3. Informe cobertura << ]")
+        #self.informeCv()
+        #self.file_df()
+        #print("[ >> fin << ]")
 
 
 if __name__ == '__main__':
